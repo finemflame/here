65bf0ad9b0b896f8224ebbfb22e8f248434891bc
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-07 17:10:21 +0200

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,29 +1,43 @@
-import { useEffect } from 'react';
+import Head from 'next/head';
 import { getPostBySlug } from 'lib/posts';
 
 export default function PostRedirect({ post }) {
-  useEffect(() => {
-    // Redirect to the WordPress domain URL with a 301 (permanent) status code
-    window.location.replace(`https://dailytrendings.info/${post.slug}`, { status: 301 });
-  }, [post]);
-
-  // Don't render anything in the component
-  return null;
+  return (
+    <Head>
+      <meta name="robots" content="noindex, nofollow" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={post.metaTitle || post.title} />
+      <meta property="og:description" content={post.og?.description || post.description} />
+      <meta property="og:image" content={post.og?.imageUrl} />
+      <meta property="og:image:secure_url" content={post.og?.imageSecureUrl} />
+      <meta property="og:image:width" content={post.og?.imageWidth} />
+      <meta property="og:image:height" content={post.og?.imageHeight} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={post.metaTitle || post.title} />
+      <meta name="twitter:description" content={post.og?.description || post.description} />
+      <meta name="twitter:image" content={post.og?.imageUrl} />
+      <meta name="twitter:image:width" content={post.og?.imageWidth} />
+      <meta name="twitter:image:height" content={post.og?.imageHeight} />
+      <meta name="article:published_time" content={post.date} />
+      <meta name="article:modified_time" content={post.modified} />
+    </Head>
+  );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params, res }) {
   const { post } = await getPostBySlug(params.slug);
 
   if (!post) {
     return {
-      props: {},
       notFound: true,
     };
   }
 
+  res.setHeader('Location', `https://dailytrendings.info/${post.slug}`);
+  res.statusCode = 301;
+  res.end();
+
   return {
-    props: {
-      post,
-    },
+    props: {},
   };
 }

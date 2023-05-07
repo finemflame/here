37ac059b21ffac328be74fb8c5b470400b432de9
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-07 17:05:24 +0200

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { useEffect } from 'react';
 import { getPostBySlug } from 'lib/posts';
 
@@ -8,26 +7,8 @@ export default function PostRedirect({ post }) {
     window.location.replace(`https://dailytrendings.info/${post.slug}`, { status: 301 });
   }, [post]);
 
-  return (
-    <Head>
-      <meta name="robots" content="noindex, nofollow" />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content={post.metaTitle || post.title} />
-      <meta property="og:description" content={post.og?.description || post.description} />
-      <meta property="og:image" content={post.og?.imageUrl} />
-      <meta property="og:image:secure_url" content={post.og?.imageSecureUrl} />
-      <meta property="og:image:width" content={post.og?.imageWidth} />
-      <meta property="og:image:height" content={post.og?.imageHeight} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={post.metaTitle || post.title} />
-      <meta name="twitter:description" content={post.og?.description || post.description} />
-      <meta name="twitter:image" content={post.og?.imageUrl} />
-      <meta name="twitter:image:width" content={post.og?.imageWidth} />
-      <meta name="twitter:image:height" content={post.og?.imageHeight} />
-      <meta name="article:published_time" content={post.date} />
-      <meta name="article:modified_time" content={post.modified} />
-    </Head>
-  );
+  // Don't render anything in the component
+  return null;
 }
 
 export async function getServerSideProps({ params }) {
